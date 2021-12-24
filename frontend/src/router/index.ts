8f5d5e14232a822a3bdc1d2bc4f2f8a58d334dc7
Linus Romland <romlandlinus@gmail.com>
2021-12-24 12:44:18 +0100

    Removed `About.vue`

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,9 +7,6 @@ const routes: Array<RouteRecordRaw> = [
 		component: () => import('../views/Home.vue')
 	},
 	{
-		path: '/about',
-		name: 'About',
-		component: () => import('../views/About.vue')
 	}
 ];
 
