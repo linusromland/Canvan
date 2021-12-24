9998ee8dbf7df9b005144acd4e5f2db422a34735
Linus Romland <romlandlinus@gmail.com>
2021-12-24 12:45:37 +0100

    Created a basic login/Logged in view on index

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
-	<router-link to="/about">About</router-link>
 	<p class="text-green-800">Home</p>
+	<p v-if="user.displayName">Logged in as: {{ user.displayName }}</p>
+	<a v-else href="/auth/google">Login</a>
 </template>
 
 <script lang="ts">
 export default {
-	name: 'Home',
-	components: {}
+	name: 'Home' as string,
+	components: {},
+	data() {
+		return {
+			user: {} as object
+		};
+	},
+	methods: {
+		async getUser() {
+			const request = await fetch('/api/user');
+			this.user = await request.json();
+		}
+	},
+	created() {
+		this.getUser();
+	}
 };
 </script>
