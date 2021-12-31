09481b19e8db114a1739ba8f9cdb343b12e8f114
Linus Romland <romlandlinus@gmail.com>
2021-12-31 18:27:51 +0100

    Changes to `navbar`

--- frontend/src/components/Navbar/index.vue
+++ frontend/src/components/Navbar/index.vue
@@ -10,10 +10,12 @@
 						</a>
 					</div>
 					<div class="flex items-center">
-						<a href="" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">Active</a>
-						<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Link</a>
-						<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Link</a>
-						<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Link</a>
+						<!-- <a href="" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">Active</a> -->
+						<div v-if="loggedIn" class="flex">
+							<p class="py-4 px-2 text-gray-900 font-semibold">Logged in as: {{ displayName }}</p>
+							<a href="/auth/logout" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Logout</a>
+						</div>
+						<router-link v-else to="/login" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Login</router-link>
 					</div>
 				</div>
 			</div>
@@ -25,7 +27,26 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-	name: 'Navbar'
+	name: 'Navbar',
+	data() {
+		return {
+			loggedIn: false as boolean,
+			displayName: '' as string
+		};
+	},
+	methods: {
+		async getUser() {
+			const request = await fetch('/api/user');
+			const user = await request.json();
+			if (user) {
+				this.loggedIn = true;
+				this.displayName = user.displayName;
+			}
+		}
+	},
+	created() {
+		this.getUser();
+	}
 });
 </script>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
 	<Navbar></Navbar>
-	<p class="text-green-800">Home</p>
-	<div v-if="loggedIn">
-		<p>Logged in as: {{ displayName }}</p>
-		<a href="/auth/logout">Logout</a>
-	</div>
-	<router-link v-else to="/login">Login</router-link>
 </template>
 
 <script lang="ts">
@@ -18,25 +12,6 @@ export default defineComponent({
 	name: 'Home' as string,
 	components: {
 		Navbar
-	},
-	data() {
-		return {
-			loggedIn: false as boolean,
-			displayName: '' as string
-		};
-	},
-	methods: {
-		async getUser() {
-			const request = await fetch('/api/user');
-			const user = await request.json();
-			if (user) {
-				this.loggedIn = true;
-				this.displayName = user.displayName;
-			}
-		}
-	},
-	created() {
-		this.getUser();
 	}
 });
 </script>
