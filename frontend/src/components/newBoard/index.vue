fbd3f2212ed16ad4e5ac679b43e7e02835fcc20d
Linus Romland <hello@linusromland.com>
2022-01-02 16:15:54 +0100

    Some styling changes for `Create Board`

--- frontend/src/components/newBoard/index.vue
+++ frontend/src/components/newBoard/index.vue
@@ -1,14 +1,17 @@
 <template>
-	<div class="fixed inset-0 w-full h-full z-50 flex bg-black" id="transition">
+	<div class="fixed inset-0 w-full h-full z-50 flex" id="transition">
 		<div class="m-auto w-2/6 bg-white p-5 rounded-md flex flex-col items-center" v-if="!createdBoard">
 			<h3 class="text-xl m-5">Create a new Kanban Board</h3>
 			<input type="text" v-model="boardName" class="w-5/6 p-2 m-5 rounded-md bg-gray-200" placeholder="Kanban Board Name" />
-			<input type="submit" class="w-5/6 p-2 rounded-md m-5 bg-blue-600 hover:bg-blue-400 text-white" value="Create" @click="createBoard" />
+			<div class="flex w-5/6 px-6">
+				<input type="submit" class="w-1/2 p-2 rounded-md m-2 bg-red-600 hover:bg-red-400 text-white" value="Cancel" @click="close" />
+				<input type="submit" class="w-1/2 p-2 rounded-md m-2 bg-blue-500 hover:bg-blue-400 text-white" value="Create" @click="createBoard" />
+			</div>
 			<p class="text-red-600">{{ errorMessage }}</p>
 		</div>
 		<div class="m-auto w-2/6 bg-white p-5 rounded-md flex flex-col items-center" v-else>
-			<h3 class="text-xl m-5">Created board "{{ boardName }}"!</h3>
-			<button type="submit" class="w-5/6 p-2 rounded-md m-5 bg-red-600 hover:bg-red-400 text-white" @click="close">Close</button>
+			<h3 class="text-2xl m-5 text-green-500">Created board "{{ boardName }}"!</h3>
+			<button type="submit" class="w-5/6 p-2 rounded-md m-5 bg-red-500 hover:bg-red-400 text-white" @click="close">Close</button>
 		</div>
 	</div>
 </template>
@@ -63,14 +66,14 @@ export default defineComponent({
 <style scoped>
 #transition {
 	animation: fadeIn 0.05s;
-	opacity: 0.5;
+	background-color: rgba(0, 0, 0, 0.5);
 }
 @keyframes fadeIn {
 	0% {
-		opacity: 0;
+		background-color: rgba(0, 0, 0, 0);
 	}
 	100% {
-		opacity: 0.5;
+		background-color: rgba(0, 0, 0, 0.5);
 	}
 }
 </style>
